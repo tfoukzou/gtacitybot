b686415d84cafe132a0e1352ce178a828a566d26
tfoukzou <109544015+tfoukzou@users.noreply.github.com>
2022-07-18 19:54:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'OTk4NjM5MzIzMjE1ODI3MDM2.G0iqfR.Cmxxt7sp8_jVoiTQDkMuTPFfT9fTc0k5oXh2Hk',
+    client.login(process.env.TOKEN),
     prefix: '+',
     owners: [971364804184928317],
     database: {
