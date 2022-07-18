6389e8d56b8d4d1b9ec77a602084edcf81c7ba26
tfoukzou <109544015+tfoukzou@users.noreply.github.com>
2022-07-18 19:53:03 +0200

    Update Confid

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'OTk4NjM5MzIzMjE1ODI3MDM2.GHKg4h.vKVH7qf4J1WGQpIs0rELAK5yc5hElSnQNx4ciE',
+    token: 'OTk4NjM5MzIzMjE1ODI3MDM2.G0iqfR.Cmxxt7sp8_jVoiTQDkMuTPFfT9fTc0k5oXh2Hk',
     prefix: '+',
     owners: [971364804184928317],
     database: {
